f782d1dd14480cfee672528ef2860cf850e860bc
Vural Sayan <vuralsayan01@gmail.com>
2023-08-12 00:05:23 +0300

    Kayıt.cs tamamlandı

---
+++ BankaTest/Kayıt.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace BankaTest
+{
+    partial class Kayıt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.MskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.MskHesapNo = new System.Windows.Forms.MaskedTextBox();
+            this.MskSifre = new System.Windows.Forms.TextBox();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SOYAD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TC KİMLİK:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "TELEFON:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "HESAP NO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ŞİFRE:";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(140, 17);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(191, 30);
+            this.TxtAd.TabIndex = 7;
+            // 
+            // MskTC
+            // 
+            this.MskTC.Location = new System.Drawing.Point(140, 111);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(191, 30);
+            this.MskTC.TabIndex = 6;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(140, 64);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(191, 30);
+            this.TxtSoyad.TabIndex = 8;
+            // 
+            // MskTelefon
+            // 
+            this.MskTelefon.Location = new System.Drawing.Point(140, 158);
+            this.MskTelefon.Mask = "(999) 000-0000";
+            this.MskTelefon.Name = "MskTelefon";
+            this.MskTelefon.Size = new System.Drawing.Size(191, 30);
+            this.MskTelefon.TabIndex = 9;
+            // 
+            // MskHesapNo
+            // 
+            this.MskHesapNo.Location = new System.Drawing.Point(140, 205);
+            this.MskHesapNo.Mask = "000000";
+            this.MskHesapNo.Name = "MskHesapNo";
+            this.MskHesapNo.Size = new System.Drawing.Size(191, 30);
+            this.MskHesapNo.TabIndex = 10;
+            this.MskHesapNo.ValidatingType = typeof(int);
+            // 
+            // MskSifre
+            // 
+            this.MskSifre.Location = new System.Drawing.Point(140, 252);
+            this.MskSifre.Name = "MskSifre";
+            this.MskSifre.Size = new System.Drawing.Size(191, 30);
+            this.MskSifre.TabIndex = 11;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(140, 299);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(191, 46);
+            this.BtnKaydet.TabIndex = 12;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(337, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 30);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Kayıt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(175)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(450, 393);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.MskSifre);
+            this.Controls.Add(this.MskHesapNo);
+            this.Controls.Add(this.MskTelefon);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Kayıt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kayıt";
+            this.Load += new System.EventHandler(this.Kayıt_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.MaskedTextBox MskTelefon;
+        private System.Windows.Forms.MaskedTextBox MskHesapNo;
+        private System.Windows.Forms.TextBox MskSifre;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ BankaTest/Kayıt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankaTest
+{
+    public partial class Kayıt : Form
+    {
+        public Kayıt()
+        {
+            InitializeComponent();
+        }
+
+        private void Kayıt_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
