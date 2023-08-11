164b64108488aeb7d81d662752083e5a995e12d0
Vural Sayan <vuralsayan01@gmail.com>
2023-08-11 23:46:22 +0300

    Giris.cs oluşturuldu

---
+++ BankaTest/Giris.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BankaTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MskHesapNo = new System.Windows.Forms.MaskedTextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.BtnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hesap No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // MskHesapNo
+            // 
+            this.MskHesapNo.Location = new System.Drawing.Point(195, 98);
+            this.MskHesapNo.Mask = "000000";
+            this.MskHesapNo.Name = "MskHesapNo";
+            this.MskHesapNo.Size = new System.Drawing.Size(191, 30);
+            this.MskHesapNo.TabIndex = 1;
+            this.MskHesapNo.ValidatingType = typeof(int);
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(195, 155);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(191, 30);
+            this.TxtSifre.TabIndex = 2;
+            // 
+            // BtnGirisYap
+            // 
+            this.BtnGirisYap.FlatAppearance.BorderSize = 0;
+            this.BtnGirisYap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGirisYap.Location = new System.Drawing.Point(195, 203);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(191, 43);
+            this.BtnGirisYap.TabIndex = 3;
+            this.BtnGirisYap.Text = "Giriş Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BtnGirisYap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 328);
+            this.Controls.Add(this.BtnGirisYap);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.MskHesapNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giriş";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox MskHesapNo;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Button BtnGirisYap;
+    }
+}
+
