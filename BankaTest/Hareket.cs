bfc3bdad3ced49986e449b4c8133298a37dc11aa
Vural Sayan <vuralsayan01@gmail.com>
2023-08-12 15:08:49 +0300

    Hareketler tablosu tamamlandı

--- BankaTest/Hareket.cs
+++ BankaTest/Hareket.cs
@@ -24,7 +24,7 @@ namespace BankaTest
         private void Hareket_Load(object sender, EventArgs e)
         {
             baglanti.Open();
-            SqlCommand komut = new SqlCommand("SELECT ID, GONDEREN,ALICI, TUTAR FROM TBLHAREKET WHERE GONDEREN=" + hesapNo, baglanti);
+            SqlCommand komut = new SqlCommand("SELECT TBLHAREKET.ID,(GON.AD + ' ' + GON.SOYAD) AS 'Gönderen',      (AL.AD + ' ' + AL.SOYAD) AS 'Alıcı',TUTAR AS 'Tutar' FROM TBLHAREKET INNER JOIN TBLKISILER AS GON ON TBLHAREKET.GONDEREN = GON.HESAPNO INNER JOIN TBLKISILER AS AL ON TBLHAREKET.ALICI = AL.HESAPNO WHERE GONDEREN=" + hesapNo, baglanti);
             SqlDataAdapter da = new SqlDataAdapter(komut);
             DataTable dt = new DataTable(); 
             da.Fill(dt);
@@ -32,7 +32,7 @@ namespace BankaTest
             baglanti.Close();
 
             baglanti.Open();
-            SqlCommand komut2 = new SqlCommand("SELECT ID, GONDEREN,ALICI, TUTAR FROM TBLHAREKET WHERE ALICI=" + hesapNo, baglanti);
+            SqlCommand komut2 = new SqlCommand("SELECT TBLHAREKET.ID, (AL.AD + ' ' + AL.SOYAD) AS 'Alıcı', (GON.AD + ' ' + GON.SOYAD) AS 'Gönderen',TUTAR AS 'Tutar' FROM TBLHAREKET INNER JOIN TBLKISILER AS GON ON TBLHAREKET.GONDEREN = GON.HESAPNO INNER JOIN TBLKISILER AS AL ON TBLHAREKET.ALICI = AL.HESAPNO WHERE ALICI=" + hesapNo, baglanti);
             SqlDataAdapter da2 = new SqlDataAdapter(komut2);
             DataTable dt2 = new DataTable();
             da2.Fill(dt2);
